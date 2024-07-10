ef8744fca573be1639b8380b424820031212ddb9
Phillip Coleman <phillip.coleman@finance-ni.gov.uk>
2024-07-10 12:39:28 +0100

    Created a header and darkmode toggle

---
+++ src/lib/components/DarkmodeButton.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import MoonIcon from '$lib/components/svg/MoonIcon.svelte';
+	import SunIcon from '$lib/components/svg/SunIcon.svelte';
+	import { darkmode } from '$lib/stores/darkmode';
+
+	function toggleDarkmode() {
+		if ($darkmode) {
+			$darkmode = false;
+			document.documentElement.classList.remove('dark');
+			localStorage.setItem('theme', 'light');
+		} else {
+			$darkmode = true;
+			document.documentElement.classList.add('dark');
+			localStorage.setItem('theme', 'dark');
+		}
+	}
+</script>
+
+<button
+	class="p-2 rounded-lg text-purple-800 hover:bg-gray-100 dark:text-yellow-200 dark:hover:bg-gray-800"
+	on:click={toggleDarkmode}
+>
+	{#if $darkmode}
+		<SunIcon class="w-8 h-8 fill-current" />
+	{:else}
+		<MoonIcon class="w-8 h-8 fill-current" />
+	{/if}
+</button>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import DarkmodeButton from '../lib/components/DarkmodeButton.svelte';
+	import IconLink from '$lib/components/IconLink.svelte';
+	import GitIcon from '$lib/components/svg/GitIcon.svelte';
+	import Logo from '$lib/components/svg/Logo.svelte';
+	import ThemeInitializer from '$lib/components/ThemeInitializer.svelte';
+	import { darkmode } from '$lib/stores/darkmode';
+	import '../app.css';
+
+	function toggleDarkmode() {
+		if ($darkmode) {
+			$darkmode = false;
+			document.documentElement.classList.remove('dark');
+			localStorage.setItem('theme', 'light');
+		} else {
+			$darkmode = true;
+			document.documentElement.classList.add('dark');
+			localStorage.setItem('theme', 'dark');
+		}
+	}
+</script>
+
+<ThemeInitializer>
+	<header
+		class="fixed z-10 top-0 inset-x-0 border-b
+        bg-white dark:bg-dark-background dark:border-gray-800"
+	>
+		<nav
+			class="flex items-center justify-between max-w-5xl
+        mx-auto px-4 h-24"
+		>
+			<a href="/">
+				<Logo class="w-16 h-16 text-gray-700 dark:text-white" />
+			</a>
+
+			<div class="flex space-x-1 items-center">
+				<DarkmodeButton />
+
+				<IconLink target="_blank" href="/">
+					<GitIcon class="w-8 h-8 fill-current" />
+				</IconLink>
+			</div>
+		</nav>
+	</header>
+
+	<main>
+		<slot />
+	</main>
+</ThemeInitializer>
