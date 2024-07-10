e4487c7fbcc678beb7f111be5b40e12ab4e0d6c7
Phillip Coleman <phillip.coleman@finance-ni.gov.uk>
2024-07-10 12:48:41 +0100

    Removed redundent code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,7 @@
 <script lang="ts">
 	import MainHeader from '../lib/components/MainHeader.svelte';
-
-	import DarkmodeButton from '../lib/components/DarkmodeButton.svelte';
-	import IconLink from '$lib/components/IconLink.svelte';
-	import GitIcon from '$lib/components/svg/GitIcon.svelte';
-	import Logo from '$lib/components/svg/Logo.svelte';
 	import ThemeInitializer from '$lib/components/ThemeInitializer.svelte';
-	import { darkmode } from '$lib/stores/darkmode';
 	import '../app.css';
-
-	function toggleDarkmode() {
-		if ($darkmode) {
-			$darkmode = false;
-			document.documentElement.classList.remove('dark');
-			localStorage.setItem('theme', 'light');
-		} else {
-			$darkmode = true;
-			document.documentElement.classList.add('dark');
-			localStorage.setItem('theme', 'dark');
-		}
-	}
 </script>
 
 <ThemeInitializer>
